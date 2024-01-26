09925ad30fe035699ec39ad57d3a602ba7d6c944
varun19424 <72430037+varun19424@users.noreply.github.com>
2024-01-26 22:44:34 +0530

    app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ I want the response in one single string having the structure
 """
 
 ## streamlit app
-st.title("Smart ATS")
+st.title("Intelligent ATS")
 st.text("Improve Your Resume ATS")
 jd=st.text_area("Paste the Job Description")
 uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
